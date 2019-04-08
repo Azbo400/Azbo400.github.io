ab4611f2adc95cf8e8f7aeea031551de3a877ebc
Ryan <azbo400@gmail.com>
2019-04-07 18:20:08 -0700

    Update Social Media Icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -48,13 +48,13 @@ class Home extends Component {
             <h3 style={{fontSize: 18, color: 'grey', margin: 0}}>PROJECTS</h3>
             <h1 style={{marginBottom: 13, fontSize: 25, color: 'white'}}>What I am currently up too.</h1>
             <div className='square-container'>
-              <ProjectBlock source='/img/mywebsite.png' info='I developed this personal website using React.js. Previous iterations of my personal web page have been made with Gatsby.' url='https://github.com/Azbo400/Azbo400.github.io'/>
+              <ProjectBlock source='/img/mywebsite.png' info='I developed this personal website using React.js. Previous iterations of my personal web page have been made with Gatsby.' url='https://github.com/ryanarnouk/ryanarnouk.github.io'/>
               <ProjectBlock source='/img/codepen.png' info='I have completed many front and back-end freeCodeCamp projects on my GitHub and on my Codepen.'
                url='https://codepen.io/azbo400/pens/popular/'/>
-              <ProjectBlock source='/img/react-redux-authentication.png' info='React Redux Authentication Biolerplate is a biolerplate application I developed that allows users to create accounts, login, logout, etc using Redux and JSON web tokens.' url='https://github.com/Azbo400/react-redux-authentication-boilerplate'/>
+              <ProjectBlock source='/img/react-redux-authentication.png' info='React Redux Authentication Biolerplate is a biolerplate application I developed that allows users to create accounts, login, logout, etc using Redux and JSON web tokens.' url='https://github.com/ryanarnouk/react-redux-authentication-boilerplate'/>
               <ProjectBlock source='/img/discover-machine-learning.png' info='Discover Machine Learning is an web app introducing machine learning to students and kids lacking a computer science background. The idea is to introduce the ever growing world of machine learning to kids.' url='http://discover-machinelearning.com'/>
-              <ProjectBlock source='/img/blogapp.png'  info='My blog app is where I occasionally write articles about my programming life, tutorials, and what I am currently up too.' url='http://azbo400.github.io/Blog'/>
-              <ProjectBlock source='/img/github.png' info='You can check out the rest of my projects on my GitHub page.' url='https://github.com/Azbo400/'/>
+              <ProjectBlock source='/img/blogapp.png'  info='My blog app is where I occasionally write articles about my programming life, tutorials, and what I am currently up too.' url='http://ryanarnouk.github.io/Blog'/>
+              <ProjectBlock source='/img/github.png' info='You can check out the rest of my projects on my GitHub page.' url='https://github.com/ryanarnouk/'/>
             </div>
           </div>
         </div>
@@ -87,7 +87,7 @@ class Home extends Component {
             <h3 style={{fontSize: 18, color: 'grey', margin: 0}}>BLOG</h3>
             <h1 style={{marginBottom: 13, fontSize: 25}}>What I am Currently Writing About.</h1>
             <Blog />
-            <p>Read more of my articles on my blog page <a href="http://azbo400.github.io/Blog">here</a></p>
+            <p>Read more of my articles on my blog page <a href="http://ryanarnouk.github.io/Blog">here</a></p>
           </div>
         </div>
         <div className="contact">
--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -6,7 +6,7 @@ class SocialMediaIcons extends Component {
     return (  
       <div className="social-icons">
         <a href="https://twitter.com/ryanarnoukk" target="_blank" rel="noopener noreferrer"><FontAwesome name="twitter"/></a>
-        <a href="https://github.com/Azbo400" target="_blank" rel="noopener noreferrer"><FontAwesome name="github" /></a>
+        <a href="https://github.com/ryanarnouk" target="_blank" rel="noopener noreferrer"><FontAwesome name="github" /></a>
         <a href="https://freecodecamp.com/azbo400" target="_blank" rel="noopener noreferrer"><FontAwesome name="free-code-camp" /></a>
         <a href="https://instagram.com/ryanarnouk" target="_blank" rel="noopener noreferrer"><FontAwesome name="instagram" /></a>
       </div>
